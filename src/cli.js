1c268e2a53999cf04d2b8e426fa73e0f8b0a87cc
itamar.ben-zaken <itamar.ben-zaken@klarna.com>
2016-12-05 10:01:25 +0200

    extracting webpack_helper

--- src/cli.js
+++ src/cli.js
@@ -7,13 +7,8 @@ import path from 'path';
 import program from 'commander';
 import chokidar from 'chokidar';
 import EventEmitter from 'events';
-import loadBabelConfig from '@kadira/storybook/dist/server/babel_config';
 import { filterStorybook } from './util';
-import loadConfig from '@kadira/storybook/dist/server/config';
-import getBaseConfig from '@kadira/storybook/dist/server/config/webpack.config.prod';
-import vm from 'vm';
-import fs from 'fs';
-import webpack from 'webpack';
+import { loadAndTweakWebpackConfig, runWebpack, evaluateStorybookConfig } from './webpack_helper';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -32,13 +27,11 @@ program
           'Path to the directory to store storyshots. Default is [config-dir]/__storyshots__')
   .option('--extension [string]', 'File extension to use for storyshot files. Default is `.shot`')
   .option('--polyfills [string]', 'Add global polyfills')
-  .option('--loaders [string]', 'Add loaders')
   .parse(process.argv);
 
 const {
   configDir = './.storybook',
   polyfills: polyfillsPath = require.resolve('./default_config/polyfills.js'),
-  loaders: loadersPath = require.resolve('./default_config/loaders.js'),
   grep,
   exclude,
 } = program;
@@ -61,29 +54,9 @@ async function main() {
     const channel = new EventEmitter();
     addons.setChannel(channel);
 
-    const config = loadConfig('PRODUCTION', getBaseConfig(), configDir);
-    config.output = {
-      path: path.resolve(configDir, './webpack'),
-      filename: 'bundle.js'
-    }
-    config.entry = path.resolve(configDir, 'config.js')
-
-    const compiler = webpack(config);
-    const stats = await new Promise((resolve, reject) => {
-      compiler.run((err, stats) => {
-        if (err)
-          reject(err)
-        else
-          resolve(stats)
-      })
-    })
-
-    const content = fs.readFileSync(path.resolve(config.output.path, config.output.filename))
-
-    require(path.resolve(polyfillsPath));
-    vm.runInThisContext(content);
-
-    const storybook = global.storybook;
+    const webpackConfig = loadAndTweakWebpackConfig(configDir)
+    const storybookConfig = await runWebpack(webpackConfig)
+    const storybook = evaluateStorybookConfig(storybookConfig, polyfillsPath)
 
     const result = await runner.run(filterStorybook(storybook, grep, exclude));
     const fails = result.errored + result.unmatched;
